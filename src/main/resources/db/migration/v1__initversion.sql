bc146a25387849ca16e59689e4628541fdfe028e
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-27 23:37:56 +0200

    #2 Database migration to keep control over the queries that interact with the Database

---
+++ src/main/resources/db/migration/v1__initversion.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employees
+(
+    id     INTEGER PRIMARY KEY ,
+    full_name VARCHAR(100) NOT NULL,
+    Email  VARCHAR(100) NOT NULL,
+    BankDetails VARCHAR(100) NOT NULL,
+    salary REAL
+);
