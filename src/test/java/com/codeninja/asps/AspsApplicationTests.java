01ba277ce24fe877e60992da1a4944c8a6dd04c3
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-27 23:39:53 +0200

    #This is the entry point to our Application.

---
+++ src/test/java/com/codeninja/asps/AspsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeninja.asps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
